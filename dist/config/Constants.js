8bfd3d4f344718159f0a783eeeb8899eb5606244
Ivo Netto <78481345+ivonetto@users.noreply.github.com>
2021-11-28 18:47:11 -0300

    Finalização da criação do Encurtador de URL, utilizando Typescript e o MongoDB

--- dist/config/Constants.js
+++ dist/config/Constants.js
@@ -1,4 +1,8 @@
-export const config = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
     API_URL: 'http://localhost:5000',
     MONGO_CONNECTION: 'mongodb+srv://ivonetto:dio@urlshortnerdio.bwrn0.mongodb.net/urlshortnerdio?retryWrites=true&w=majority'
-}
+};
+//# sourceMappingURL=Constants.js.map
